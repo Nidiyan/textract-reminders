fcd323c8ca0d2b94835f6c8c696489230b0c2970
Nidiyan <36421994+Nidiyan@users.noreply.github.com>
2022-05-17 20:21:39 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/installsh | bash
-.~/.nvm/nvm.sh
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
 nvm install node
 
-DIR="/home/ec2-user/textract-reminders"
-
-if ([-d "$DIR"]); then
-    echo "${DIR}" exists"
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/express-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
 else
-    echo "Creating ${DIR}"
-    mkdir ${DIR}
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
 fi
