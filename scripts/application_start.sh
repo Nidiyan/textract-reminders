48bc5810c0b8528398a82074ec7511411b1bba67
Nidiyan <36421994+Nidiyan@users.noreply.github.com>
2022-05-17 20:23:12 -0500

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/textract-reminders
+
+#navigate into our working directory where we have all our github files
 cd /home/ec2-user/textract-reminders
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-# Install packages
+#install node modules
 npm install
 
-# Start node app in the background
-node server.js > app.out.log 2> app.err.log < /dev/null &
+#start our node app in the background
+node server.js > app.out.log 2> app.err.log < /dev/null & 
